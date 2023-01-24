6447e50caa5c47cb5e57de0ca866bd16abffe8bd
Björn.Ettelman <bjet2100@student.miun.se>
2023-01-24 15:53:20 +0100

    Add project files.

---
+++ Moment 1.1/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Moment_1._1.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
